26a2fe8eb86d119a81778e302e7276df28c03639
Sri Mohan <dob.hoy@gmail.com>
2015-03-31 11:16:50 +0100

    Initial Commit

---
+++ AlcolatorAgain/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AlcolatorAgain
+//
+//  Created by Srikanth Narayanamohan on 31/03/2015.
+//  Copyright (c) 2015 Sri Mohan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
